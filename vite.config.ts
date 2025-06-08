5ef5fd179533ff44c34bf4c50e5496b29f6722a3
xeqtr-zhg <xeqtr.zhg@gmail.com>
2025-06-08 19:38:02 +0400

    add setting lint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint2';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   plugins: [
     eslintPlugin({
       include: ['src/**/*.ts', 'src/**/*.tsx'],
     }),
+    checker({ typescript: true }),
   ],
   build: {
     lib: {
