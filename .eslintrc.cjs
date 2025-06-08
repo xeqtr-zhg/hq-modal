dbabc4e7f809fd1f234e8092e6e9d231b0c8fca1
xeqtr-zhg <xeqtr.zhg@gmail.com>
2025-06-08 13:49:38 +0400

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {},
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint2';
 
 export default defineConfig({
+  plugins: [
+    eslintPlugin({
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+    }),
+  ],
   build: {
     lib: {
       entry: './src/index.ts',
       name: 'hq-modal',
       fileName: (format) => `hq-modal.${format}.js`,
-      formats: ['es', 'cjs', 'umd']
-    }
-  }
-})
+      formats: ['es', 'cjs', 'umd'],
+    },
+  },
+});
